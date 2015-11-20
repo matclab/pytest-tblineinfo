980bac17cb5e2ea67ae3be68112b88e34cd886f1
Mathieu Clabaut <mathieu.clabaut@gmail.com>
2015-11-20 10:18:14 +0100

    Doc and package improvement

--- setup.py
+++ setup.py
@@ -4,14 +4,16 @@ import os
 del os.link
 
 from setuptools import setup
+import codecs
 
 setup(
     name="pytest-tblineinfo",
-    version = "0.1",
+    version = "0.3",
     description = (
         'tblineinfo is a py.test plugin that insert the node id in the final '
         'py.test report when --tb=line option is used'
     ),
+    long_description=codecs.open("README.rst", encoding='utf-8').read(),
     author = 'Mathieu Clabaut',
     author_email = 'mathieu.clabaut@systerel.fr, mathieu@clabaut.net',
     py_modules=['pytest_tblineinfo'],
@@ -21,10 +23,12 @@ setup(
     install_requires=[
                 'pytest>=2.0',
             ],
+    license='MIT',
+    url='https://github.com/matclab/pytest-tblineinfo',
     classifiers=[
                 'Development Status :: 3 - Alpha',
                 'Intended Audience :: Developers',
-                'License :: OSI Approved :: BSD License',
+                'License :: OSI Approved :: MIT License',
                 'Operating System :: POSIX',
                 'Operating System :: Microsoft :: Windows',
                 'Operating System :: MacOS :: MacOS X',
